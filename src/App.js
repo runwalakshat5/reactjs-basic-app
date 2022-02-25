b72efd17b59adb1a9de52da340d618986612c49a
runwalakshat5 <runwalakshat5@gmail.com>
2022-02-25 18:00:53 +0530

    Added  initial react files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import Greet from "./components/1.function/Greet";
+import { Named as Akshat } from "./components/1.function/Named";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greet></Greet>
+     <Akshat></Akshat>
+      
     </div>
   );
 }
---
+++ src/components/1.function/Named.js
@@ -0,0 +1,5 @@
+import React from "react";
+export const Named = () => {
+    return <h1>Default component</h1>
+ }
+
